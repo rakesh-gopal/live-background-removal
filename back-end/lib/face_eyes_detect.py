d9e4c5fb37b4357cdaba455f10ffa9dd14436393
Rakesh P Gopal <rapadinj@microsoft.com>
2019-08-03 20:25:53 +0530

    Added the face-detect script

--- back-end/lib/face_eyes_detect.py
+++ back-end/lib/face_eyes_detect.py
@@ -13,6 +13,11 @@ import sys
 # images. 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
+# https://github.com/Itseez/opencv/blob/master 
+# /data/haarcascades/haarcascade_eye.xml 
+# Trained XML file for detecting eyes 
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
+
 source = 0   					# defaults to local (laptop) camera
 
 if len(sys.argv) > 1:
@@ -40,6 +45,15 @@ while True:
     for (x,y,w,h) in faces: 
         # To draw a rectangle in a face 
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = img[y:y+h, x:x+w] 
+
+        # Detects eyes of different sizes in the input image 
+        eyes = eye_cascade.detectMultiScale(roi_gray) 
+
+        #To draw a rectangle in eyes 
+        for (ex,ey,ew,eh) in eyes: 
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2) 
 
     # Display an image in a window 
     cv2.imshow('img',img) 
